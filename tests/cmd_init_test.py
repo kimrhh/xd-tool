e3b8fa1bcb1b4cba691f57782bdcbd062d5985bf
Kim Højgaard-Hansen <kimrhh@exherbo.org>
2014-11-08 14:19:12 +0100

    add tests for init cmd

--- tests/cmd_init_test.py
+++ tests/cmd_init_test.py
@@ -1,15 +1,13 @@
 from xd.tool.main import main
 import xd.tool.log
+from xd.tool.shell import call
 
 from nose.tools import raises, with_setup
 import unittest
-import sys
 import os
-import stat
 import tempfile
 import shutil
-import sh
-from redirect import *
+from redirect import stdchannel_redirected
 
 
 class tests(unittest.case.TestCase):
@@ -25,45 +23,27 @@ class tests(unittest.case.TestCase):
         os.chdir(self.oldcwd)
         if self.oldpwd:
             os.environ['PWD'] = self.oldpwd
-        shutil.rmtree(self.testdir, ignore_errors=True)
+        shutil.rmtree(self.testdir)
         xd.tool.log.deinit()
 
     def test_init(self):
-        with stdchannels_redirected():
+        with stdchannel_redirected(1):
             self.assertEqual(main(['xd', 'init']), None)
         self.assertTrue(os.path.isdir('.git'))
         self.assertTrue(os.path.isfile('.xd'))
 
     def test_init_twice(self):
-        with stdchannels_redirected():
+        with stdchannel_redirected(1):
             self.assertEqual(main(['xd', 'init']), None)
         self.assertTrue(os.path.isdir('.git'))
         self.assertTrue(os.path.isfile('.xd'))
-        with stdchannels_redirected():
+        with stdchannel_redirected(2):
             self.assertEqual(main(['xd', '-d', 'init']), None)
         self.assertTrue(os.path.isdir('.git'))
         self.assertTrue(os.path.isfile('.xd'))
 
-    def test_init_fail_1(self):
+    def test_init_fail(self):
         with open('.git', 'w') as f:
-            f.write('foobar')
-        with stdchannels_redirected():
-            self.assertNotEqual(main(['xd', 'init']), None)
-
-    def test_init_fail_2(self):
-        with open('.git', 'w') as f:
-            f.write('foobar')
-        with open('.xd', 'w') as f:
             f.write('foobar')
         self.assertNotEqual(main(['xd', 'init']), None)
-
-    def test_init_fail_3(self):
-        os.chmod('.', stat.S_IREAD|stat.S_IEXEC)
-        with stdchannels_redirected():
-            self.assertNotEqual(main(['xd', 'init']), None)
-
-    def test_init_fail_4(self):
-        sh.git.init()
-        os.chmod('.git/refs/heads', stat.S_IREAD)
-        with stdchannels_redirected():
-            self.assertNotEqual(main(['xd', 'init']), None)
+        
